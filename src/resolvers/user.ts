037f41ae8deaae836e6faaeca3d51c3521888f43
ChannelJuanNews <jruel006@ucr.edu>
2021-02-24 15:56:01 -0800

    can now pseudo login with either email or username

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,5 +1,13 @@
 import { User } from "../entities/User";
-import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql";
+import {
+  Arg,
+  Ctx,
+  Field,
+  Mutation,
+  ObjectType,
+  Query,
+  Resolver,
+} from "type-graphql";
 import { MyContext } from "../types";
 
 import { LOGGER, ERROR } from "../util/logger";
@@ -7,6 +15,24 @@ import { LOGGER, ERROR } from "../util/logger";
 import argon2 from "argon2";
 
 import validateEmail from "../util/validateEmail";
+
+@ObjectType()
+class UserError {
+  @Field()
+  message!: String;
+
+  @Field()
+  code!: number;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => UserError, { nullable: true })
+  error?: UserError;
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
 
 @Resolver()
 export class UserResolver {
@@ -23,33 +49,86 @@ export class UserResolver {
   }
 
   // create/register user
-  @Mutation(() => User || Object, { nullable: true })
+  @Mutation(() => UserResponse, { nullable: true })
   async registerUser(
     @Arg("email") email: string,
     @Arg("username") username: string,
     @Arg("password") password: string,
     @Ctx() { em }: MyContext
-  ) {
-    LOGGER("WE ARE HERE");
+  ): Promise<UserResponse> {
     // make sure that the email is valid
     if (validateEmail(email)) {
-      LOGGER("WE ARE HERE, VALIDATED");
       const hashed = await argon2.hash(password);
-      const newUser = await em.create(User, {
-        email,
-        username,
-        password: hashed,
-      });
 
-      await em.persistAndFlush(newUser);
-      return newUser;
+      try {
+        const newUser = await em.create(User, {
+          email,
+          username,
+          password: hashed,
+        });
+
+        await em.persistAndFlush(newUser);
+        LOGGER("NEW USER CREATED");
+        return {
+          user: newUser,
+        };
+      } catch (e) {
+        switch (String(e.code)) {
+          case "23505":
+            LOGGER("DUPLICATE KEY ERROR");
+            return {
+              error: {
+                message: "Username is already taken",
+                code: 40,
+              },
+            };
+            break;
+          default:
+            return {
+              error: {
+                message: "Error authenticating",
+                code: 40,
+              },
+            };
+        }
+      }
     } else {
       ERROR("ERROR CANNOT VALIDATE EMAIL => ", email);
       return {
-        error: true,
-        error_message: "Please provide a valid email address",
+        error: {
+          message: "Please provide a valid email",
+          code: 40,
+        },
       };
     }
+  }
+
+  // log a user in
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg("email") email: string,
+    @Arg("username") username: string,
+    @Arg("password") password: string,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    const user = await em.findOne(User, { $or: [{ email }, { username }] });
+
+    if (user) {
+      const verified = argon2.verify(user.password, password);
+      if (verified) {
+        return {
+          user: user,
+        };
+      }
+    }
+
+    return {
+      error: {
+        message:
+          "Please provide a valid username or email and correct password",
+        code: 40,
+      },
+    };
   }
 
   // update user by id
